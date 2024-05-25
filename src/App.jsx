7980f17c488d0c091f76c65568818aff1b758cae
negxative <89023129+negxative@users.noreply.github.com>
2024-05-26 00:42:16 +0530

    implementation of infinite scroll with cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { setData } from './Store/ContentSlice'
+import JobCardsContainer from './Components/JobBoard/JobCardsContainer'
+import { getSampleJdJSON } from './Assets/SampleData'
 
 function App() {
+  const data=getSampleJdJSON();
+  console.log(data)
   return (
     <>
-      hello weekday !!!!
+      <JobCardsContainer data={data}/>
     </>
   )
 }
